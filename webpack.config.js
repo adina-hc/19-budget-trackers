fd0cfa0fa0ba058172dc4d7111943756501d9458
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-23 09:17:33 -0400

    configured webpack and modified package-lock.json; included script in index.html

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// Configure the bundle package for webpack
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  mode: "development",
+};
+
+module.exports = config;
